bf1c3f68136318ab487b47e9637bbb0c01eb9d41
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-08 09:33:31 +0000

    Added helmet, cors and logger

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,9 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
     client: 'sqlite3',
-    useNullAsDefault: true, // needed for sqlite
+    useNullAsDefault: true,
     connection: {
-      filename: './data/recipes.db3',
+      filename: './data/projects.db3',
     },
     migrations: {
       directory: './data/migrations'
@@ -14,11 +11,9 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     },
-    // add the following
     pool: {
       afterCreate: (conn, done) => {
-        // runs after a connection is made to the sqlite engine
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+        conn.run('PRAGMA foreign_keys = ON', done);
       },
     },
   }, 
